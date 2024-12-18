d36e0c65879f1bb30a55097cecb789b3bda3e84b
Jeffrey Toniolo <6921365+jtoniolo@users.noreply.github.com>
2024-12-17 21:26:43 -0500

    Refactor tests to improve coverage and update state management logic

--- src/apps/ui/src/app/app.component.spec.ts
+++ src/apps/ui/src/app/app.component.spec.ts
@@ -2,19 +2,17 @@ import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { RouterModule } from '@angular/router';
+import { AppFacade } from './+state/app.facade';
+import { provideMockStore, MockStore } from '@ngrx/store/testing';
 
 describe('AppComponent', () => {
+  const initialState = { title: 'ui' };
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AppComponent, NxWelcomeComponent, RouterModule.forRoot([])],
+      providers: [AppFacade, provideMockStore({})],
     }).compileComponents();
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Welcome ui');
   });
 
   it(`should have as title 'ui'`, () => {
