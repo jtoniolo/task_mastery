53c825664db1622fae83e5842a797cc9ee8369ca
Jeffrey Toniolo <6921365+jtoniolo@users.noreply.github.com>
2025-01-17 13:20:35 -0500

    Enhance Dashboard service to retrieve detailed email statistics including unread counts, top senders, and emails by age; update DTOs to reflect new data structure.

--- apps/api/src/dashboard/dashboard.service.ts
+++ apps/api/src/dashboard/dashboard.service.ts
@@ -4,7 +4,11 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { MongoRepository } from 'typeorm';
 
 import { Message } from '../gmail/entities/message.entity';
-import { DashboardDto } from './dto/dashboard.dto';
+import {
+  DashboardDto,
+  LabelCountDto,
+  SenderCountDto,
+} from './dto/dashboard.dto';
 
 @Injectable({ scope: Scope.REQUEST })
 export class DashboardService {
@@ -15,9 +19,11 @@ export class DashboardService {
     @Inject(REQUEST)
     private readonly request: Request,
   ) {}
+
   private getUserId(): string {
     return this.request['user'].id;
   }
+
   private get userId(): string {
     return this.request['user'].id ?? '';
   }
@@ -25,19 +31,301 @@ export class DashboardService {
   async getDashboardData(): Promise<DashboardDto> {
     try {
       const messageCount = await this.getMessageCount();
-      this.logger.debug(`Found ${messageCount} messages for user`);
-      return { messageCount };
+      const unreadMessageCount = await this.getUnreadMessageCount();
+      const topTenSenderCount = await this.getTopTenSenderCount();
+      const topTenSenderDomainCount = await this.getTopTenSenderDomainCount();
+      const topTenLabelsCount = await this.getTopTenLabelsCount();
+
+      const emailsOlderThan10Years = await this.getEmailsOlderThanYears(10);
+      const emailsOlderThan5Years =
+        await this.getEmailsOlderThanYearsButLessThanYears(5, 10);
+      const emailsOlderThan3Years =
+        await this.getEmailsOlderThanYearsButLessThanYears(3, 5);
+      const emailsOlderThan1Year =
+        await this.getEmailsOlderThanYearsButLessThanYears(1, 3);
+      const emailsOlderThan6Months =
+        await this.getEmailsOlderThanMonthsButLessThanYears(6, 1);
+      const emailsOlderThan3Months =
+        await this.getEmailsOlderThanMonthsButLessThanMonths(3, 6);
+      const emailsOlderThan1Month =
+        await this.getEmailsOlderThanMonthsButLessThanMonths(1, 3);
+
+      this.logger.debug(`Found ${unreadMessageCount} unread messages for user`);
+      return {
+        messageCount,
+        unreadMessageCount,
+        topTenSenderCount,
+        topTenSenderDomainCount,
+        topTenLabelsCount,
+        emailsOlderThan10Years,
+        emailsOlderThan5Years,
+        emailsOlderThan3Years,
+        emailsOlderThan1Year,
+        emailsOlderThan6Months,
+        emailsOlderThan3Months,
+        emailsOlderThan1Month,
+      };
     } catch (error) {
       this.logger.error(`Error getting dashboard data for user`, error);
       throw error;
     }
   }
-
   async getMessageCount(): Promise<number> {
     try {
       return await this.messageRepository.countBy({ userId: this.userId });
     } catch (error) {
       this.logger.error(`Error getting message count for user`, error);
+      throw error;
+    }
+  }
+  async getUnreadMessageCount(): Promise<number> {
+    try {
+      return await this.messageRepository.countBy({
+        userId: this.userId,
+        labelIds: 'UNREAD',
+      });
+    } catch (error) {
+      this.logger.error(`Error getting unread message count for user`, error);
+      throw error;
+    }
+  }
+
+  async getEmailsOlderThanYears(years: number): Promise<number> {
+    const date = new Date();
+    date.setFullYear(date.getFullYear() - years);
+    try {
+      return await this.messageRepository.countBy({
+        userId: this.userId,
+        date: { $lt: date },
+      });
+    } catch (error) {
+      this.logger.error(
+        `Error getting emails older than ${years} years for user`,
+        error,
+      );
+      throw error;
+    }
+  }
+
+  async getEmailsOlderThanYearsButLessThanYears(
+    minYears: number,
+    maxYears: number,
+  ): Promise<number> {
+    const minDate = new Date();
+    minDate.setFullYear(minDate.getFullYear() - maxYears);
+    const maxDate = new Date();
+    maxDate.setFullYear(maxDate.getFullYear() - minYears);
+    try {
+      return await this.messageRepository.countBy({
+        userId: this.userId,
+        date: { $gte: minDate, $lt: maxDate },
+      });
+    } catch (error) {
+      this.logger.error(
+        `Error getting emails older than ${minYears} years but less than ${maxYears} years for user`,
+        error,
+      );
+      throw error;
+    }
+  }
+
+  async getEmailsOlderThanMonthsButLessThanYears(
+    minMonths: number,
+    maxYears: number,
+  ): Promise<number> {
+    const minDate = new Date();
+    minDate.setFullYear(minDate.getFullYear() - maxYears);
+    const maxDate = new Date();
+    maxDate.setMonth(maxDate.getMonth() - minMonths);
+    try {
+      return await this.messageRepository.countBy({
+        userId: this.userId,
+        date: { $gte: minDate, $lt: maxDate },
+      });
+    } catch (error) {
+      this.logger.error(
+        `Error getting emails older than ${minMonths} months but less than ${maxYears} years for user`,
+        error,
+      );
+      throw error;
+    }
+  }
+
+  async getEmailsOlderThanMonthsButLessThanMonths(
+    minMonths: number,
+    maxMonths: number,
+  ): Promise<number> {
+    const minDate = new Date();
+    minDate.setMonth(minDate.getMonth() - maxMonths);
+    const maxDate = new Date();
+    maxDate.setMonth(maxDate.getMonth() - minMonths);
+    try {
+      return await this.messageRepository.countBy({
+        userId: this.userId,
+        date: { $gte: minDate, $lt: maxDate },
+      });
+    } catch (error) {
+      this.logger.error(
+        `Error getting emails older than ${minMonths} months but less than ${maxMonths} months for user`,
+        error,
+      );
+      throw error;
+    }
+  }
+
+  async getTopTenSenderCount(): Promise<SenderCountDto[]> {
+    try {
+      const values = await this.messageRepository
+        .aggregate(
+          [
+            {
+              // Match the documents where the userId is the current user AND the from field does not contain the user's email address.
+              $match: {
+                userId: this.userId,
+                from: {
+                  $not: {
+                    $regex: this.request['user'].email,
+                    $options: 'i',
+                  },
+                },
+              },
+            },
+            {
+              $group: {
+                _id: '$from',
+                count: {
+                  $count: {},
+                },
+              },
+            },
+            {
+              $sort: {
+                count: -1,
+              },
+            },
+            {
+              $limit: 10,
+            },
+          ],
+          {},
+        )
+        .project<{ _id: string; count: number }>({ _id: 1, count: 1 })
+        .toArray();
+
+      return values.map((value) => ({
+        sender: value._id,
+        count: value.count,
+      }));
+    } catch (error) {
+      this.logger.error(`Error getting top ten sender count for user`, error);
+      throw error;
+    }
+  }
+
+  async getTopTenSenderDomainCount(): Promise<SenderCountDto[]> {
+    try {
+      const values = await this.messageRepository
+        .aggregate(
+          [
+            {
+              // Match the documents where the userId is the current user AND the from field does not contain the user's email address.
+              $match: {
+                userId: this.userId,
+                from: {
+                  $not: {
+                    $regex: this.request['user'].email,
+                    $options: 'i',
+                  },
+                },
+              },
+            },
+            {
+              $group: {
+                _id: {
+                  $arrayElemAt: [
+                    {
+                      $split: ['$from', '@'],
+                    },
+                    1,
+                  ],
+                },
+                count: {
+                  $count: {},
+                },
+              },
+            },
+            {
+              $sort: {
+                count: -1,
+              },
+            },
+            {
+              $limit: 10,
+            },
+          ],
+          {},
+        )
+        .project<{ _id: string; count: number }>({ _id: 1, count: 1 })
+        .toArray();
+
+      return values.map((value) => ({
+        sender: value._id,
+        count: value.count,
+      }));
+    } catch (error) {
+      this.logger.error(
+        `Error getting top ten sender domain count for user`,
+        error,
+      );
+      throw error;
+    }
+  }
+
+  async getTopTenLabelsCount(): Promise<LabelCountDto[]> {
+    try {
+      const values = await this.messageRepository
+        .aggregate(
+          [
+            {
+              // Match the documents where the userId is the current user. and not SENT, INBOX, UNREAD
+              $match: {
+                userId: this.userId,
+                labelIds: {
+                  $nin: ['SENT', 'INBOX', 'UNREAD'],
+                },
+              },
+            },
+            {
+              $unwind: '$labelIds',
+            },
+            {
+              $group: {
+                _id: '$labelIds',
+                count: {
+                  $count: {},
+                },
+              },
+            },
+            {
+              $sort: {
+                count: -1,
+              },
+            },
+            {
+              $limit: 10,
+            },
+          ],
+          {},
+        )
+        .project<{ _id: string; count: number }>({ _id: 1, count: 1 })
+        .toArray();
+
+      return values.map((value) => ({
+        labelId: value._id,
+        count: value.count,
+      }));
+    } catch (error) {
+      this.logger.error(`Error getting top ten labels count for user`, error);
       throw error;
     }
   }
--- apps/api/src/dashboard/dto/dashboard.dto.ts
+++ apps/api/src/dashboard/dto/dashboard.dto.ts
@@ -1,9 +1,129 @@
 import { ApiProperty } from '@nestjs/swagger';
+export class SenderCountDto {
+  /**
+   * The sender's email address.
+   */
+  @ApiProperty()
+  sender?: string;
+
+  /**
+   * The count of emails sent by the sender.
+   */
+  @ApiProperty()
+  count?: number;
+}
+
+export class LabelCountDto {
+  /**
+   * The label's name.
+   */
+  @ApiProperty()
+  labelId?: string;
+
+  /**
+   * The count of emails with the label.
+   */
+  @ApiProperty()
+  count?: number;
+}
+
+export class CountByDateRangeDto {
+  /**
+   * The groupings name.
+   * @ex 'Older than 5 years'
+   */
+  @ApiProperty()
+  name: string;
+
+  /**
+   * The start date of the date
+   */
+  @ApiProperty()
+  startDate: string;
+
+  /**
+   * The end date of the date
+   */
+  @ApiProperty()
+  endDate: string;
+
+  /**
+   * The count of emails in the date range.
+   */
+  @ApiProperty()
+  count: number;
+}
+
 export class DashboardDto {
   /**
    * The user's email count.
    */
-
   @ApiProperty()
-  messageCount?: number;
+  messageCount: number;
+
+  /**
+   * The user's unread email count.
+   */
+  @ApiProperty()
+  unreadMessageCount?: number;
+
+  /**
+   * The user's top ten email senders.
+   */
+  @ApiProperty({ type: [SenderCountDto] })
+  topTenSenderCount: SenderCountDto[];
+
+  /**
+   * The user's top ten email sender domains.
+   */
+  @ApiProperty({ type: [SenderCountDto] })
+  topTenSenderDomainCount: SenderCountDto[];
+
+  /**
+   * The user's top ten email labels.
+   */
+  @ApiProperty({ type: [LabelCountDto] })
+  topTenLabelsCount: LabelCountDto[];
+
+  /**
+   * Count of emails older than 10 years.
+   */
+  @ApiProperty()
+  emailsOlderThan10Years?: number;
+
+  /**
+   * Count of emails older than 5 years, but less than 10 years.
+   */
+  @ApiProperty()
+  emailsOlderThan5Years?: number;
+
+  /**
+   * Count of emails older than 3 years, but less than 5 years.
+   */
+  @ApiProperty()
+  emailsOlderThan3Years?: number;
+
+  /**
+   * Count of emails older than 1 year, but less than 3 years.
+   */
+  @ApiProperty()
+  emailsOlderThan1Year?: number;
+
+  /**
+   * Count of emails older than 6 months, but less than 1 year.
+   */
+  @ApiProperty()
+  emailsOlderThan6Months?: number;
+
+  /**
+   * Count of emails older than 3 months, but less than 6 months.
+   */
+  @ApiProperty()
+  emailsOlderThan3Months?: number;
+
+  /**
+   * Count of emails older than 1 month, but less than 3 months.
+   */
+  @ApiProperty()
+  emailsOlderThan1Month?: number;
 }
