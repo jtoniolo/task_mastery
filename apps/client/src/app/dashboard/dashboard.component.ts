205ef722aa469f808a5d148781b58386ad166cbc
Jeffrey Toniolo <6921365+jtoniolo@users.noreply.github.com>
2025-01-17 17:20:25 -0500

    Enhance Dashboard component to display detailed email statistics, including summary, top senders, sender domains, and labels; update styles for improved layout.

--- apps/client/src/app/dashboard/dashboard.component.ts
+++ apps/client/src/app/dashboard/dashboard.component.ts
@@ -1,11 +1,14 @@
 import { Component, inject, OnInit } from '@angular/core';
+import { MatBadgeModule } from '@angular/material/badge';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
-import { MatBadgeModule } from '@angular/material/badge';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatTableModule } from '@angular/material/table';
 import { AppFacade } from '../+state/app.facade';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { CommonModule } from '@angular/common';
 import { DashboardFacade } from '../+state';
+import { map } from 'rxjs';
 @Component({
   selector: 'tsm-dashboard',
   imports: [
@@ -14,6 +17,8 @@ import { DashboardFacade } from '../+state';
     CommonModule,
     MatIconModule,
     MatBadgeModule,
+    MatGridListModule,
+    MatTableModule,
   ],
   providers: [DashboardFacade],
   templateUrl: './dashboard.component.html',
@@ -26,8 +31,52 @@ export class DashboardComponent implements OnInit {
   }
   readonly #facade = inject(AppFacade);
   readonly #dashboardFacade = inject(DashboardFacade);
+  displayedColumns: string[] = ['title', 'value'];
+
   profile = toSignal(this.#facade.profile$);
-  dashboard = toSignal(this.#dashboardFacade.dashboard$);
+  dashboard = toSignal(
+    this.#dashboardFacade.dashboard$.pipe(
+      map((dashboard) => {
+        return {
+          summary: [
+            { title: 'Total Messages', value: dashboard?.messageCount },
+            { title: 'Unread Messages', value: dashboard?.unreadMessageCount },
+            {
+              title: 'Emails Older Than 10 Years',
+              value: dashboard?.emailsOlderThan10Years,
+            },
+            {
+              title: 'Emails Older Than 5 Years',
+              value: dashboard?.emailsOlderThan5Years,
+            },
+            {
+              title: 'Emails Older Than 3 Years',
+              value: dashboard?.emailsOlderThan3Years,
+            },
+            {
+              title: 'Emails Older Than 1 Year',
+              value: dashboard?.emailsOlderThan1Year,
+            },
+            {
+              title: 'Emails Older Than 6 Months',
+              value: dashboard?.emailsOlderThan6Months,
+            },
+            {
+              title: 'Emails Older Than 3 Months',
+              value: dashboard?.emailsOlderThan3Months,
+            },
+            {
+              title: 'Emails Older Than 1 Month',
+              value: dashboard?.emailsOlderThan1Month,
+            },
+          ],
+          topTenSenders: dashboard?.topTenSenderCount,
+          topTenSenderDomains: dashboard?.topTenSenderDomainCount,
+          topTenLabels: dashboard?.topTenLabelsCount,
+        };
+      }),
+    ),
+  );
 
   load() {
     this.#facade.dashboardOpen();
