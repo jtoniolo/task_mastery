b3862146a2042df5ff8bce83b6ac5c6e08214f4f
Jeffrey Toniolo <6921365+jtoniolo@users.noreply.github.com>
2025-01-19 15:05:40 -0500

    Update proxy classes

--- apps/client/src/app/proxy/model/models.ts
+++ apps/client/src/app/proxy/model/models.ts
@@ -3,5 +3,6 @@ export * from './healthControllerCheckV1200Response';
 export * from './healthControllerCheckV1200ResponseInfoValue';
 export * from './healthControllerCheckV1503Response';
 export * from './labelCountDto';
+export * from './labelDto';
 export * from './senderCountDto';
 export * from './userDto';
