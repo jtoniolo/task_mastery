d6444d2187232b266fe44001a7eb0036f1da1161
Jeffrey Toniolo <6921365+jtoniolo@users.noreply.github.com>
2025-01-20 15:43:18 -0500

    Enhance DialogComponent tests; add MAT_DIALOG_DATA and MatDialogRef providers for better test coverage

--- apps/client/src/app/dialog/dialog.component.spec.ts
+++ apps/client/src/app/dialog/dialog.component.spec.ts
@@ -1,17 +1,28 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DialogComponent } from './dialog.component';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 
 describe('DialogComponent', () => {
   let component: DialogComponent;
   let fixture: ComponentFixture<DialogComponent>;
+  let dialogData = {
+    title: 'Dialog Title',
+    message: 'Dialog Message',
+    type: 'Alert',
+  };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [DialogComponent],
+      providers: [
+        { provide: MatDialogRef, useValue: {} },
+        { provide: MAT_DIALOG_DATA, useValue: dialogData },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(DialogComponent);
+
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
