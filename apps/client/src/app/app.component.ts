b05363c405fdc75b2d46bfa4d01a93d8368a562a
Jeffrey Toniolo <6921365+jtoniolo@users.noreply.github.com>
2025-01-25 10:08:13 -0500

    work in progress. building out header and menu layout.

--- apps/client/src/app/app.component.ts
+++ apps/client/src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { AppFacade } from './+state/app.facade';
 
 @Component({
   selector: 'tsm-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    MatButtonModule,
+    MatIconModule,
+    MatListModule,
+    MatSidenavModule,
+    MatToolbarModule,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
@@ -15,4 +27,18 @@ export class AppComponent implements OnInit {
     // Initialize the app state!
     this.#facade.init();
   }
+
+  links = [
+    {
+      route: 'dashboard',
+      isActive: true,
+      label: 'Dashboard',
+      icon: 'dashboard',
+    },
+    { route: 'tasks', isActive: false, label: 'Tasks', icon: 'list' },
+    { route: 'projects', isActive: false, label: 'Projects', icon: 'work' },
+    { route: 'profile', isActive: false, label: 'Profile', icon: 'person' },
+  ];
+
+  showInfo(link: any): void {}
 }
---
+++ apps/client/src/app/nav-menu/nav-menu.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'tsm-nav-menu',
+  imports: [],
+  templateUrl: './nav-menu.component.html',
+  styleUrl: './nav-menu.component.scss'
+})
+export class NavMenuComponent {
+
+}
